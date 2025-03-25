dee35ee1124797aa0a45f3dcde7c05d6803739b7
Gabriel Bauer <gabeb1277@gmail.com>
2025-03-25 14:30:33 -0400

    Opt. Alloc.

--- opt_malloc.c
+++ opt_malloc.c
@@ -3,146 +3,475 @@
 #include <sys/mman.h>
 #include <pthread.h>
 #include <string.h>
-
+#include <stdio.h>
+#ifdef __linux__
+#include <sys/personality.h>
+#endif
 #include "xmalloc.h"
 
-// Memory allocator by Kernighan and Ritchie,
-// The C programming Language, 2nd ed.  Section 8.7.
-//
-// Then copied from xv6.
-//
-// Then modified to use mmap and add a mutex by Nat Tuck, becoming starter code
-// for CS3650 in 2020.
-//
-// Note that this does not count as starter code for HW08, just for CH02.
-
-typedef long Align;
-
-union header {
-  struct {
-    union header *ptr;
-    unsigned int size;
-  } s;
-  Align x;
-};
-
-typedef union header Header;
-
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-static Header base;
-static Header *freep;
+
+typedef struct size24_block {
+	size_t size;
+	struct size24_block *next;
+	size_t _unused;
+} size24_block;
+
+typedef struct size32_block {
+	size_t size;
+	struct size32_block *next;
+	size_t _unused[2];
+} size32_block;
+
+typedef struct size64_block {
+	size_t size;
+	struct size64_block *next;
+	size_t _unused[6];
+} size64_block;
+
+typedef struct size72_block {
+	size_t size;
+	struct size72_block *next;
+	size_t _unused[7];
+} size72_block;
+
+typedef struct size128_block {
+	size_t size;
+	struct size128_block *next;
+	size_t _unused[14];
+} size128_block;
+
+typedef struct size256_block {
+	size_t size;
+	struct size256_block *next;
+	size_t _unused[30];
+} size256_block;
+
+typedef struct size512_block {
+	size_t size;
+	struct size512_block *next;
+	size_t _unused[62];
+} size512_block;
+
+typedef struct size1024_block {
+	size_t size;
+	struct size1024_block *next;
+	size_t _unused[126];
+} size1024_block;
+
+typedef struct size2048_block {
+	size_t size;
+	struct size2048_block *next;
+	size_t _unused[254];
+} size2048_block;
+
+static pthread_mutex_t lock_24s = PTHREAD_MUTEX_INITIALIZER;
+static size24_block* size24s = 0;
+
+void* size24_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size24s == 0) {
+		size24s = (size24_block*)point;
+	} else {
+		size24_block* tr = (size24_block*)point;
+		tr->next = size24s;
+		size24s = tr;
+	}
+	return 0;
+}
+
+void* size24_malloc() {
+	if (size24s == 0) {
+		size24_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 170; ii++) {
+			size24_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size24s;
+	size24s = size24s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_32s = PTHREAD_MUTEX_INITIALIZER;
+static size32_block* size32s = 0;
+
+void* size32_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size32s == 0) {
+		size32s = (size32_block*)point;
+	} else {
+		size32_block* tr = (size32_block*)point;
+		tr->next = size32s;
+		size32s = tr;
+	}
+	return 0;
+}
+
+void* size32_malloc() {
+	if (size32s == 0) {
+		size32_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 127; ii++) {
+			size32_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size32s;
+	size32s = size32s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_64s = PTHREAD_MUTEX_INITIALIZER;
+static size64_block* size64s = 0;
+
+void* size64_free(void* ptr) {
+	if (size64s == 0) {
+		size64s = (size64_block*)ptr;
+	} else {
+		size64_block* pntr = (size64_block*)ptr;
+		pntr->next = size64s;
+		size64s = pntr;
+	}
+	return 0;
+}
+
+void* size64_malloc() {
+	if (size64s == 0) {
+		size64_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 63; ii++) {
+			size64_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size64s;
+	size64s = size64s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_72s = PTHREAD_MUTEX_INITIALIZER;
+static size72_block* size72s = 0;
+
+void* size72_free(void* ptr) {
+	if (size72s == 0) {
+		size72s = (size72_block*)ptr;
+	} else {
+		size72_block* pntr = (size72_block*)ptr;
+		pntr->next = size72s;
+		size72s = pntr;
+	}
+	return 0;
+}
+
+void* size72_malloc() {
+	if (size72s == 0) {
+		size72_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 56; ii++) {
+			size72_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size72s;
+	size72s = size72s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_128s = PTHREAD_MUTEX_INITIALIZER;
+static size128_block* size128s = 0;
+
+void* size128_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size128s == 0) {
+		size128s = (size128_block*)point;
+	} else {
+		size128_block* tr = (size128_block*)point;
+		tr->next = size128s;
+		size128s = tr;
+	}
+	return 0;
+}
+
+void* size128_malloc() {
+	if (size128s == 0) {
+		size128_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 31; ii++) {
+			size128_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size128s;
+	size128s = size128s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_256s = PTHREAD_MUTEX_INITIALIZER;
+static size256_block* size256s = 0;
+
+void* size256_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size256s == 0) {
+		size256s = (size256_block*)point;
+	} else {
+		size256_block* tr = (size256_block*)point;
+		tr->next = size256s;
+		size256s = tr;
+	}
+	return 0;
+}
+
+void* size256_malloc() {
+	if (size256s == 0) {
+		size256_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 15; ii++) {
+			size256_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size256s;
+	size256s = size256s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_512s = PTHREAD_MUTEX_INITIALIZER;
+static size512_block* size512s = 0;
+
+void* size512_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size512s == 0) {
+		size512s = (size512_block*)point;
+	} else {
+		size128_block* tr = (size128_block*)point;
+		tr->next = size128s;
+		size128s = tr;
+	}
+	return 0;
+}
+
+void* size512_malloc() {
+	if (size512s == 0) {
+		size512_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 7; ii++) {
+			size512_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size512s;
+	size512s = size512s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_1024s = PTHREAD_MUTEX_INITIALIZER;
+static size1024_block* size1024s = 0;
+
+void* size1024_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size1024s == 0) {
+		size1024s = (size1024_block*)point;
+	} else {
+		size1024_block* tr = (size1024_block*)point;
+		tr->next = size1024s;
+		size1024s = tr;
+	}
+	return 0;
+}
+
+void* size1024_malloc() {
+	if (size1024s == 0) {
+		size1024_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 3; ii++) {
+			size1024_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size1024s;
+	size1024s = size1024s->next;
+	return ptr + 1;
+}
+
+static pthread_mutex_t lock_2048s = PTHREAD_MUTEX_INITIALIZER;
+static size2048_block* size2048s = 0;
+
+void* size2048_free(void* ptr) {
+	size_t* point = ptr - 1;
+	if (size2048s == 0) {
+		size2048s = (size2048_block*)point;
+	} else {
+		size2048_block* tr = (size2048_block*)point;
+		tr->next = size2048s;
+		size2048s = tr;
+	}
+	return 0;
+}
+
+void* size2048_malloc() {
+	if (size2048s == 0) {
+		size2048_block* page = xmalloc(4088);
+		for (int ii = 0; ii < 1; ii++) {
+			size2048_free(&(page[ii]));
+		}
+	}
+	
+	size_t* ptr = (void*)size2048s;
+	size2048s = size2048s->next;
+	return ptr + 1;
+}
 
 static
 void
 xfree_helper(void *ap)
 {
-  Header *bp, *p;
-
-  bp = (Header*)ap - 1;
-  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-    if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-      break;
-  if(bp + bp->s.size == p->s.ptr){
-    bp->s.size += p->s.ptr->s.size;
-    bp->s.ptr = p->s.ptr->s.ptr;
-  } else
-    bp->s.ptr = p->s.ptr;
-  if(p + p->s.size == bp){
-    p->s.size += bp->s.size;
-    p->s.ptr = bp->s.ptr;
-  } else
-    p->s.ptr = bp;
-  freep = p;
+	size_t* ptr = (size_t*)ap - 1;
+	munmap(ptr, *ptr);
 }
 
 void
 xfree(void* ap)
 {
-  pthread_mutex_lock(&lock);
-  xfree_helper(ap);
-  pthread_mutex_unlock(&lock);
-}
-
-static Header*
-morecore(size_t nu)
-{
-  char *p;
-  Header *hp;
-
-  if(nu < 4096)
-    nu = 4096;
-  p = mmap(0, nu * sizeof(Header), PROT_READ|PROT_WRITE,
-           MAP_ANONYMOUS|MAP_SHARED, -1, 0);
-  if(p == (char*)-1)
-    return 0;
-  hp = (Header*)p;
-  hp->s.size = nu;
-  xfree_helper((void*)(hp + 1));
-  return freep;
+  size_t *ptr = (size_t*)ap - 1;
+  if (*ptr == 24) {
+  	pthread_mutex_lock(&lock_24s);
+  	size24_free(ap);
+  	pthread_mutex_unlock(&lock_24s);
+  }
+  else if (*ptr == 32) {
+  	pthread_mutex_lock(&lock_32s);
+  	size32_free(ap);
+  	pthread_mutex_unlock(&lock_32s);
+  }
+  else if (*ptr == 64) {
+  	pthread_mutex_lock(&lock_64s);
+  	size64_free(ap);
+  	pthread_mutex_unlock(&lock_64s);
+  }
+  else if (*ptr == 128) {
+  	pthread_mutex_lock(&lock_128s);
+  	size128_free(ap);
+  	pthread_mutex_unlock(&lock_128s);
+  }
+  else if (*ptr == 256) {
+  	pthread_mutex_lock(&lock_256s);
+  	size256_free(ap);
+  	pthread_mutex_unlock(&lock_256s);
+  }
+  else if (*ptr == 512) {
+  	pthread_mutex_lock(&lock_512s);
+  	size512_free(ap);
+  	pthread_mutex_unlock(&lock_512s);
+  }
+  else if (*ptr == 1024) {
+  	pthread_mutex_lock(&lock_1024s);
+  	size1024_free(ap);
+  	pthread_mutex_unlock(&lock_1024s);
+  }
+  else if (*ptr == 2048) {
+  	pthread_mutex_lock(&lock_2048s);
+  	size2048_free(ap);
+  	pthread_mutex_unlock(&lock_2048s);
+  }
+  else if (*ptr == 72) {
+  	pthread_mutex_lock(&lock_72s);
+  	size72_free(ap);
+  	pthread_mutex_unlock(&lock_72s);
+  }
+  else {
+  	pthread_mutex_lock(&lock);
+  	xfree_helper(ap);
+  	pthread_mutex_unlock(&lock);
+  }
 }
 
 void*
 xmalloc_helper(size_t nbytes)
 {
-  Header *p, *prevp;
-  unsigned int nunits;
-
-  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
-  if((prevp = freep) == 0){
-    base.s.ptr = freep = prevp = &base;
-    base.s.size = 0;
-  }
-  
-  for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
-    if(p->s.size >= nunits){
-      if(p->s.size == nunits)
-        prevp->s.ptr = p->s.ptr;
-      else {
-        p->s.size -= nunits;
-        p += p->s.size;
-        p->s.size = nunits;
-      }
-      freep = prevp;
-      return (void*)(p + 1);
-    }
-    if(p == freep) {
-      if((p = morecore(nunits)) == 0) {
-        return 0;
-      }
-    }
-  }
+  #ifdef __linux__
+  personality(ADDR_NO_RANDOMIZE);
+  #endif
+  //printf("xmalloc(%ld)\n", nbytes);
+  size_t* block = mmap(0, nbytes, PROT_READ|PROT_WRITE,
+           MAP_ANONYMOUS|MAP_SHARED, -1, 0);
+  *block = nbytes;
+  return block + 1;
 }
 
 void*
 xmalloc(size_t nbytes)
 {
-  pthread_mutex_lock(&lock);
-  void *ret = xmalloc_helper(nbytes);
-  pthread_mutex_unlock(&lock);
-  return ret;
+  nbytes += sizeof(size_t);
+  //printf("xmalloc(%ld)\n", nbytes);
+  if (nbytes == 24) {
+  	pthread_mutex_lock(&lock_24s);
+  	void *ptr = size24_malloc();
+  	pthread_mutex_unlock(&lock_24s);
+  	return ptr;
+  }
+  else if (nbytes == 32) {
+  	pthread_mutex_lock(&lock_32s);
+  	void *ptr = size32_malloc();
+  	pthread_mutex_unlock(&lock_32s);
+  	return ptr;
+  }
+  else if (nbytes == 64) {
+  	pthread_mutex_lock(&lock_64s);
+  	void *ptr = size64_malloc();
+  	pthread_mutex_unlock(&lock_64s);
+  	return ptr;
+  }
+  else if (nbytes == 72) {
+  	pthread_mutex_lock(&lock_72s);
+  	void *ptr = size72_malloc();
+  	pthread_mutex_unlock(&lock_72s);
+  	return ptr;
+  }
+  else if (nbytes == 128) {
+  	pthread_mutex_lock(&lock_128s);
+  	void *ptr = size128_malloc();
+  	pthread_mutex_unlock(&lock_128s);
+  	return ptr;
+  }
+  else if (nbytes == 256) {
+  	pthread_mutex_lock(&lock_256s);
+  	void *ptr = size256_malloc();
+  	pthread_mutex_unlock(&lock_256s);
+  	return ptr;
+  }
+  else if (nbytes == 512) {
+  	pthread_mutex_lock(&lock_512s);
+  	void *ptr = size512_malloc();
+  	pthread_mutex_unlock(&lock_512s);
+  	return ptr;
+  }
+  else if (nbytes == 1024) {
+  	pthread_mutex_lock(&lock_1024s);
+  	void *ptr = size1024_malloc();
+  	pthread_mutex_unlock(&lock_1024s);
+  	return ptr;
+  }
+  else if (nbytes == 2048) {
+  	pthread_mutex_lock(&lock_2048s);
+  	void *ptr = size2048_malloc();
+  	pthread_mutex_unlock(&lock_2048s);
+  	return ptr;
+  }
+  else {
+  	pthread_mutex_lock(&lock);
+  	void *ret = xmalloc_helper(nbytes);
+  	pthread_mutex_unlock(&lock);
+  	return ret;
+  }
 }
 
 void*
 xrealloc(void* prev, size_t nn)
 {
-  pthread_mutex_lock(&lock);
-  Header *p;
-
-  p = (Header*)prev - 1;
-  int nunits = p->s.size;
   // TODO: implement a working realloc
   // This is where we are failing the next test...
-  void *new = xmalloc_helper(nn);
+  void *new = xmalloc(nn);
+  size_t *ptr = (size_t*)prev-1;
   
   memset(new, 0, nn);
-  // nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1
-  int nbytes = nunits*(sizeof(Header)+1) - sizeof(Header) + 1; // In reverse...
-  memcpy(new, prev, nbytes);
+  memcpy(new, prev, *ptr);
   
-  xfree_helper(prev);
+  xfree(prev);
   
-  //Header *bp = (Header*)new - 1;
-  pthread_mutex_unlock(&lock);
   return new;
 }
 
